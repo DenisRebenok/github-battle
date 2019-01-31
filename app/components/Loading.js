a9692f9c2b6836aa7ba9831b27f2fa9d88c5beba
Denys Rebenok <denis.rebenok.pro@gmail.com>
2019-01-31 20:18:38 +0200

    Refactor to use Class Properties (Class Fields)

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -9,13 +9,17 @@ var styles = {
 };
 
 class Loading extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      text: props.text
-    };
-  }
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    speed: PropTypes.number.isRequired
+  };
+  static defaultProps = {
+    text: "Loading",
+    speed: 300
+  };
+  state = {
+    text: this.props.text
+  };
   componentDidMount() {
     var stopper = this.props.text + "...";
     this.interval = window.setInterval(
@@ -42,15 +46,5 @@ class Loading extends React.Component {
     return <p style={styles.content}>{this.state.text}</p>;
   }
 }
-
-Loading.propTypes = {
-  text: PropTypes.string.isRequired,
-  speed: PropTypes.number.isRequired
-};
-
-Loading.defaultProps = {
-  text: "Loading",
-  speed: 300
-};
 
 export default Loading;
