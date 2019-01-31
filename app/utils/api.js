72f1ef59c8bad65576e1f5b5dc9fe7561b6da2a4
Denys Rebenok <denis.rebenok.pro@gmail.com>
2019-01-31 02:13:50 +0200

    install query-string package

--- app/utils/api.js
+++ app/utils/api.js
@@ -23,7 +23,7 @@ function getRepos(username) {
 }
 
 function getStarCount(repos) {
-  return repos.data.reduce(function(count, repos) {
+  return repos.data.reduce(function(count, repo) {
     return count + repo.stargazers_count;
   }, 0);
 }
