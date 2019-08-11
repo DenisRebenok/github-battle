eb33d87d10a7639656cd167bbc5c8e4d0bab28d4
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-08-11 13:01:08 +0300

    404

--- app/components/App.js
+++ app/components/App.js
@@ -23,17 +23,13 @@ class App extends React.Component {
           <div id="app" className={this.state.theme}>
             <div className="container">
               <Nav />
-              <Route exact path="/" component={Popular} />
-              <Route exact path="/battle" component={Battle} />
-              <Route path="/battle/results" component={Results} />
+              <Switch>
+                <Route exact path="/" component={Popular} />
+                <Route exact path="/battle" component={Battle} />
+                <Route path="/battle/results" component={Results} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
             </div>
-            {/* <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/battle" component={Battle} />
-              <Route path="/battle/results" component={Results} />
-              <Route path="/popular" component={Popular} />
-              <Route render={() => <p>Not Found</p>} />
-            </Switch> */}
           </div>
         </ThemeProvider>
       </Router>
