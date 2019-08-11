dff3cc83d55ff79718cc5efbe880b2ffd7a3b9df
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-08-11 04:24:07 +0300

    add Routes

--- app/components/App.js
+++ app/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Popular from './Popular'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Nav from './Nav'
-import Home from './Home'
+// import Home from './Home'
 import Battle from './Battle'
 import Results from './Results'
 import { ThemeProvider } from '../contexts/theme'
@@ -18,23 +18,24 @@ class App extends React.Component {
   }
   render() {
     return (
-      <ThemeProvider value={this.state}>
-        {/* <Router> */}
-        <div id="app" className={this.state.theme}>
-          <div className="container">
-            <Nav />
-            <Popular />
-          </div>
-          {/* <Switch>
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div id="app" className={this.state.theme}>
+            <div className="container">
+              <Nav />
+              <Route exact path="/" component={Popular} />
+              <Route path="/battle" component={Battle} />
+            </div>
+            {/* <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/battle" component={Battle} />
               <Route path="/battle/results" component={Results} />
               <Route path="/popular" component={Popular} />
               <Route render={() => <p>Not Found</p>} />
             </Switch> */}
-        </div>
-        {/* </Router> */}
-      </ThemeProvider>
+          </div>
+        </ThemeProvider>
+      </Router>
     )
   }
 }
